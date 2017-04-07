8d4cc782eee3849c18b7a7936b86e241cdceb5f0
zbo14 <zbalder68@gmail.com>
2017-04-07 16:39:15 -0500

    adding menu template

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const FormData = require('form-data')
 const http = require('http')
 
-let win
-
-app.on('ready', () => {
-  win = new BrowserWindow({'width': 1000, 'height': 1000})
-  win.loadURL(`file://${__dirname}/templates/login-register.html`)
-  win.webContents.openDevTools()
-  win.on('closed', () => {
-    win = null
-  })
-})
-
 var host = 'localhost', port = 8888
 
-exports.httpRequest = (callback, headers, method, path) => {
+function httpRequest(callback, headers, method, path) {
   let options = {
     hostname: host,
     method: method,
@@ -32,10 +21,136 @@ exports.httpRequest = (callback, headers, method, path) => {
   return req
 }
 
-exports.formatJSON = (data) => {
+function formatJSON(data) {
   return JSON.stringify(JSON.parse(data), null, 2)
 }
 
-exports.navigate = (page) => {
+function navigate(page) {
   win.loadURL(`file://${__dirname}/templates/` + page + '.html')
 }
+
+const template = [
+  {
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      /* {role: 'services', submenu: []}, */
+      {
+        label: 'navigate',
+        submenu: [
+          {
+            label: 'discovery',
+            submenu: [
+              {
+                label: 'query',
+                click() { navigate('query') }
+              },
+              {
+                label: 'search',
+                click() { navigate('search') }
+              }
+            ]
+          },
+          {
+            label: 'metadata',
+            submenu: [
+              {
+                label: 'composition',
+                click() { navigate('composition') }
+              },
+              {
+                label: 'recording',
+                click() { navigate('recording') }
+              }
+            ]
+          },
+          {
+            label: 'ownership',
+            submenu: [
+              {
+                label: 'license',
+                click() { navigate('license') }
+              },
+              {
+                label: 'right',
+                click() { navigate('right') }
+              }
+            ]
+          },
+          {
+            label: 'verfication',
+            submenu: [
+              {
+                label: 'prove',
+                click() { navigate('prove') }
+              },
+              {
+                label: 'verify',
+                click() { navigate('verify') }
+              }
+            ]
+          }
+        ]
+      },
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'reload'},
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {role: 'minimize'},
+      {role: 'close'}
+    ]
+  }
+]
+
+let menu, win
+
+app.on('ready', () => {
+  menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  win = new BrowserWindow({'width': 1000, 'height': 1000})
+  win.loadURL(`file://${__dirname}/templates/login-register.html`)
+  // win.webContents.openDevTools()
+  win.on('closed', () => {
+    win = null
+  })
+})
+
+exports.formatJSON = formatJSON
+exports.httpRequest = httpRequest
+exports.navigate = navigate
